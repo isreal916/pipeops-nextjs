e3481e27f5dbc26c22120f80fa0ece25047d67c1
isreal916 <isrealadeoluwa89@gmail.com>
2024-06-13 22:59:13 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,9 +98,9 @@ export default function Home() {
       </div>
       <div className="flex justify-center bg-white flex-col space-y-4">
       <h4 className=" mt-[80px] max-w-[36rem] text-3xl font-bold  text-slate-900 self-center font-poppins "> Main Feature</h4>
-       <p className="self-center font-medium font-sans text-[#606060] w-[45rem]">By targeting the hospitality industry MedMind can help establishments elevate their service,build brand loyalty,and attract health-conscious customers who appreciate personalized attention to their nutritional needs.</p>
+       <p className="self-center font-medium font-sans text-[#606060] w-[50rem]">By targeting the hospitality industry MedMind can help establishments elevate their service,build brand loyalty,and attract health-conscious customers who appreciate personalized attention to their nutritional needs.</p>
 
-      </div>
+      </div><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
 
     </>
   );
